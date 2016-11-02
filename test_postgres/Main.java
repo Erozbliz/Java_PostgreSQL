65ba259221fb0535e3729257a706d651521bd12f
Erozbliz <erozbliz@hotmail.com>
2016-11-02 00:01:15 -0400

    Version de base
    Contient la connexion à postgreSQL.
    Nécessite la bonne BDD et les logins

---
+++ test_postgres/Main.java
@@ -0,0 +1,58 @@
+package test_postgres;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Main test Postgress");
+		//Connexion();
+		Select();
+
+	}
+
+	public static void Connexion() {
+		Connection c = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "root");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+	}
+
+	public static void Select() {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "root");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM opus_user;");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				System.out.println("ID = " + id);
+				System.out.println("NAME = " + name);
+				System.out.println();
+			}
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Operation done successfully");
+	}
+
+}
