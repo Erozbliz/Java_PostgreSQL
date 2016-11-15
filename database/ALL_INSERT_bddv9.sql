49d3e77947e251c5fa15d8132810d3f1e35e36b6
Erozbliz <erozbliz@hotmail.com>
2016-11-15 17:55:09 -0500

    ajout test recursif

--- database/ALL_INSERT_bddv9.sql
+++ database/ALL_INSERT_bddv9.sql
@@ -34,3 +34,46 @@ INSERT INTO public.composition(id_node, id_node_1) VALUES (3, 12);
 INSERT INTO public.appartient(id_root, id_node) VALUES (1, 1);
 INSERT INTO public.appartient(id_root, id_node) VALUES (1, 2);
 INSERT INTO public.appartient(id_root, id_node) VALUES (1, 3);
+
+
+
+
+
+--AUTRES POUR TEST
+INSERT INTO public.places(place, is_in) VALUES (1000, 300);
+INSERT INTO public.places(place, is_in) VALUES (300, 90);
+INSERT INTO public.places(place, is_in) VALUES (200, 90);
+INSERT INTO public.places(place, is_in) VALUES (100, 90);
+INSERT INTO public.places(place, is_in) VALUES (90, 1);
+INSERT INTO public.places(place, is_in) VALUES (80, 1);
+INSERT INTO public.places(place, is_in) VALUES (70, 1);
+INSERT INTO public.places(place, is_in) VALUES (60, 2);
+INSERT INTO public.places(place, is_in) VALUES (50, 2);
+INSERT INTO public.places(place, is_in) VALUES (40, 2);
+INSERT INTO public.places(place, is_in) VALUES (30, 2);
+
+--http://www.openscg.com/2014/08/hierarchical-queries-using-postgresql/
+--http://bender.io/2013/09/22/returning-hierarchical-data-in-a-single-sql-query/
+
+--Récursive 1
+WITH RECURSIVE q AS (SELECT place, is_in
+                       FROM places
+                      WHERE place = 90
+                      UNION ALL
+                     SELECT m.place, m.is_in
+                       FROM places m
+                       JOIN q ON q.place = m.is_in)
+        SELECT place, is_in FROM q
+
+
+
+
+--Récursive 2
+WITH RECURSIVE q AS (SELECT place, is_in
+                                 FROM places
+                                WHERE place = 90
+                                UNION
+                               SELECT m.place, m.is_in
+                                 FROM places m
+                                 JOIN q ON q.place = m.is_in)
+                  SELECT place, is_in FROM q;
