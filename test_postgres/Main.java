92fb12565788d5ce37708f5f594847eaffdc57c7
Erozbliz <erozbliz@hotmail.com>
2016-11-15 17:26:12 -0500

    Adapter pour la v9

--- test_postgres/Main.java
+++ test_postgres/Main.java
@@ -2,13 +2,11 @@ package test_postgres;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -51,13 +49,11 @@ public class Main {
 				String name = rs.getString("name");
 				String tag = rs.getString("tag");
 				int size = rs.getInt("size");
-				int id_root = rs.getInt("id_root");
 
 				System.out.print(" id_node = " + id_node);
 				System.out.print(" name = " + name);
 				System.out.print(" tag = " + tag);
 				System.out.print(" size = " + size);
-				System.out.print(" id_root = " + id_root);
 				System.out.println();
 			}
 			rs.close();
@@ -75,13 +71,8 @@ public class Main {
 			stmt = c.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM composition;");
 			while (rs.next()) {
-				Boolean niveau_un = rs.getBoolean("niveau_un");
-				int root = rs.getInt("id_root");
 				int id_parent = rs.getInt("id_node");
 				int id_enfant = rs.getInt("id_node_1");
-
-				System.out.print(" niveau_un = " + niveau_un);
-				System.out.print(" root = " + root);
 				System.out.print(" id_parent = " + id_parent);
 				System.out.print(" id_enfant = " + id_enfant);
 				System.out.println();
@@ -97,8 +88,6 @@ public class Main {
 
 	public static void SelectAll(Connection c, int numRoot) {
 
-		//liste 
-		ArrayList<Integer> listNodeDeRoot = new ArrayList<Integer>();
 		//param1 = enfant , parent2 = parent
 		Map<Integer, Integer> mapEnfantParent = new HashMap<Integer, Integer>();
 		HashMap<Integer, Object> mapEnfantParent2 = new HashMap<Integer, Object>();
@@ -107,24 +96,22 @@ public class Main {
 
 		//POUR LES COMPOSITION
 		try {
-			PreparedStatement prepareQuery;
+			/*PreparedStatement prepareQuery;
 			prepareQuery = c.prepareStatement("SELECT * FROM composition WHERE id_root = ?");
 			prepareQuery.setLong(1, numRoot);
-			ResultSet rs = prepareQuery.executeQuery();
+			ResultSet rs = prepareQuery.executeQuery();*/
+
+			Statement stmt = null;
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM composition;");
 			while (rs.next()) {
-				Boolean niveau_un = rs.getBoolean("niveau_un");
-				int root = rs.getInt("id_root");
 				int id_parent = rs.getInt("id_node");
 				int id_enfant = rs.getInt("id_node_1");
 
-				if (niveau_un == true) {
-					listNodeDeRoot.add(id_enfant);
-				} else {
-					listParent.add(id_parent);
-					listEnfant.add(id_enfant);
-					mapEnfantParent.put(id_enfant, id_parent);
-					mapEnfantParent2.put(id_enfant, id_parent);
-				}
+				listParent.add(id_parent);
+				listEnfant.add(id_enfant);
+				mapEnfantParent.put(id_enfant, id_parent);
+				mapEnfantParent2.put(id_enfant, id_parent);
 
 				/*System.out.print(" niveau_un = " + niveau_un);
 				System.out.print(" root = " + root);
@@ -139,22 +126,23 @@ public class Main {
 
 		//POUR LES NOEUDS
 		try {
-			PreparedStatement prepareQuery2;
-			prepareQuery2 = c.prepareStatement("SELECT * FROM NODE WHERE id_root = ?");
+			/*PreparedStatement prepareQuery2;
+			prepareQuery2 = c.prepareStatement("SELECT * FROM NODE");
 			prepareQuery2.setLong(1, numRoot);
-			ResultSet rs = prepareQuery2.executeQuery();
+			ResultSet rs = prepareQuery2.executeQuery();*/
+			Statement stmt = null;
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM node;");
 			while (rs.next()) {
 				int id_node = rs.getInt("id_node");
 				String name = rs.getString("name");
 				String tag = rs.getString("tag");
 				int size = rs.getInt("size");
-				int id_root = rs.getInt("id_root");
 
 				System.out.print(" id_node = " + id_node);
 				System.out.print(" name = " + name);
 				System.out.print(" tag = " + tag);
 				System.out.print(" size = " + size);
-				System.out.print(" id_root = " + id_root);
 				System.out.println();
 			}
 		} catch (SQLException e) {
@@ -162,15 +150,9 @@ public class Main {
 		}
 
 		//--------Test (pas bon)
-		/*for (int i = 0; i < listNodeDeRoot.size(); i++) {
-			int idMasterNode = listNodeDeRoot.get(i);
-			System.out.println("-" + idMasterNode);
-			for (int j = 0; j < listParent.size(); j++) {
-				if (idMasterNode == listParent.get(j)) {
-					System.out.println("--- " + listParent.get(j) + " - " + listEnfant.get(j));
-				}
-			}
-		}*/
+		for (int i = 0; i < mapEnfantParent.size(); i++) {
+
+		}
 
 		//---------TEST 1
 		/*Set<Integer> keys = mapEnfantParent.keySet(); //key=enfant ,parent
@@ -183,46 +165,54 @@ public class Main {
 			}
 		}*/
 
-		//---------TEST 2
-		for (int i = 0; i < listNodeDeRoot.size(); i++) {
-			System.out.println("-" + listNodeDeRoot.get(i));
-			recursiveFindLeaf(listNodeDeRoot.get(i), mapEnfantParent);
-		}
-		System.out.println("\n");
-		iterateHashMap(mapEnfantParent2);
-
+		recursiveFindLeaf(mapEnfantParent);
 		System.out.println("Operation done successfully");
 	}
 
 	//---------TEST 2
-	public static void recursiveFindLeaf(int numNodeFromRoot, Map<Integer, Integer> mapEnfantParent) {
+	public static void recursiveFindLeaf(Map<Integer, Integer> mapEnfantParent) {
 		Set<Integer> keys = mapEnfantParent.keySet(); //key=enfant ,parent
 		for (Integer key : keys) {
-			if (numNodeFromRoot == mapEnfantParent.get(key)) {
-				System.out.println("---" + mapEnfantParent.get(key) + "<->" + key);
-				//mapEnfantParent.remove(key);
-				//recursiveFindLeaf(numNodeFromRoot,mapEnfantParent);
-			} else {
-				//mapEnfantParent.remove(key);
-				//recursiveFindLeaf(numNodeFromRoot,mapEnfantParent);
-				//return;
-			}
+			System.out.println("---" + mapEnfantParent.get(key) + "<->" + key);
+			//mapEnfantParent.remove(key);
+			//recursiveFindLeaf(numNodeFromRoot,mapEnfantParent);
+
+			//mapEnfantParent.remove(key);
+			//recursiveFindLeaf(numNodeFromRoot,mapEnfantParent);
+			//return;
+
 		}
+		
+		// { 1 :[{1,2,3}],2...
+		int gardeMyVar =1;
+		System.out.print("{");
+		for (Integer key : keys) {
+			
+			if(mapEnfantParent.get(key)==gardeMyVar){
+				System.out.print("{" + key+"}");
+			}else{
+				System.out.print(mapEnfantParent.get(key));
+				gardeMyVar=mapEnfantParent.get(key);
+			}
+			//System.out.print(",{");
+			
+		}
+		System.out.print("}");
 	}
 
 	// http://www.prodigyproductionsllc.com/articles/programming/recursively-iterate-hashmap-with-java/
 	// http://stackoverflow.com/questions/9337536/iterate-recursively-through-deep-hashmap
 	//-------TEST 3 récursive 
-	private static void iterateHashMap(HashMap<Integer, Object> map) {
+	/*private static void iterateHashMap(HashMap<Integer, Object> map) {
 		Iterator it = map.keySet().iterator();
 		System.out.println("-");
 		while (it.hasNext()) {
 			Integer key = (Integer) it.next();
 			System.out.println("---" + map.get(key) + "<->" + key);
-			if (map.get(key) instanceof HashMap){
+			if (map.get(key) instanceof HashMap) {
 				iterateHashMap((HashMap) map.get(key));
 			}
 		}
-	}
+	}*/
 
 }
