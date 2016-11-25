efdae2ccff40df065d7c5c46fa5922b1106262f6
Erozbliz <erozbliz@hotmail.com>
2016-11-24 23:30:37 -0500

    Maj méthode à terminer
    Ajout de la composition 0 reliée à 1 2 3

--- database/ALL_INSERT_bddv9.sql
+++ database/ALL_INSERT_bddv9.sql
@@ -1,6 +1,7 @@
 --Sunburst
 INSERT INTO public.sunburst(id_root, title) VALUES (1, 'UdeS');
 -- Création des noeud
+INSERT INTO public.node(id_node, name, tag, size) VALUES (0, 'UdeS root', 'root', 50);
 INSERT INTO public.node(id_node, name, tag, size) VALUES (1, 'GEI771', 'security', 50);
 INSERT INTO public.node(id_node, name, tag, size) VALUES (2, 'GEI772', 'security', 50);
 INSERT INTO public.node(id_node, name, tag, size) VALUES (3, 'GEI773', 'security', 50);
@@ -19,6 +20,9 @@ INSERT INTO public.node(id_node, name, tag, size) VALUES (11, 'APP2 773', 'secur
 INSERT INTO public.node(id_node, name, tag, size) VALUES (12, 'APP3 773', 'security 773', 50);
 
 --Création des liaison entre les nodes (parent,enfant)
+INSERT INTO public.composition(id_node, id_node_1) VALUES (0, 1);
+INSERT INTO public.composition(id_node, id_node_1) VALUES (0, 2);
+INSERT INTO public.composition(id_node, id_node_1) VALUES (0, 3);
 INSERT INTO public.composition(id_node, id_node_1) VALUES (1, 4);
 INSERT INTO public.composition(id_node, id_node_1) VALUES (1, 5);
 INSERT INTO public.composition(id_node, id_node_1) VALUES (1, 6);
--- test_postgres/Main.java
+++ test_postgres/Main.java
@@ -20,7 +20,6 @@ public class Main {
 			//SelectNode(c);
 			//SelectComposition(c);
 			SelectAll(c, 1);
-			//recursiveSql(c);
 			c.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -96,13 +95,8 @@ public class Main {
 		ArrayList<Integer> listParent = new ArrayList<Integer>();
 		ArrayList<Integer> listEnfant = new ArrayList<Integer>();
 
-		//POUR LES COMPOSITION
+		//POUR LES COMPOSITION on remplit le hasmap
 		try {
-			/*PreparedStatement prepareQuery;
-			prepareQuery = c.prepareStatement("SELECT * FROM composition WHERE id_root = ?");
-			prepareQuery.setLong(1, numRoot);
-			ResultSet rs = prepareQuery.executeQuery();*/
-
 			Statement stmt = null;
 			stmt = c.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM composition;");
@@ -126,12 +120,8 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		//POUR LES NOEUDS
+		//POUR LES NOEUDS on remplit le hasmap
 		try {
-			/*PreparedStatement prepareQuery2;
-			prepareQuery2 = c.prepareStatement("SELECT * FROM NODE");
-			prepareQuery2.setLong(1, numRoot);
-			ResultSet rs = prepareQuery2.executeQuery();*/
 			Statement stmt = null;
 			stmt = c.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM node;");
@@ -151,76 +141,24 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		//--------Test (pas bon)
-		for (int i = 0; i < mapEnfantParent.size(); i++) {
 
-		}
-
-		//---------TEST 1
-		/*Set<Integer> keys = mapEnfantParent.keySet(); //key=enfant ,parent
-		for (int i = 0; i < listNodeDeRoot.size(); i++) {
-			System.out.println("-" + listNodeDeRoot.get(i));
-			for (Integer key : keys) {
-				if (listNodeDeRoot.get(i) == mapEnfantParent.get(key)) {
-					System.out.println("---" + mapEnfantParent.get(key) + "<->" + key);
-				}
-			}
-		}*/
-
-		recursiveFindLeaf(mapEnfantParent);
+		//recursiveFindLeaf(mapEnfantParent);
 		System.out.println("\n---------------");
-		/*List<Object> test = recursiveFindLeafFinal(mapEnfantParent2);
-		for(Object room : test) {
-			  System.out.println(room);
-		}*/
 		//System.out.print("{'name':'"+1+"','children':[");
 		FinalRecursivee(1,mapEnfantParent);
-		String replacedString = finalJson.replace("}{", "},{");
-		System.out.println("\n"+replacedString);
+		String replacedString = finalJson.replace("}{", "},{");// a déplacer dans une nouvelle méthode <<<<<<
+		System.out.println("\n"+replacedString);// a déplacer dans une nouvelle méthode <<<<<<
+		//utilser http://www.jsoneditoronline.org/ 
 	}
 
-	//---------TEST 2
-	public static void recursiveFindLeaf(Map<Integer, Integer> mapEnfantParent) {
-		System.out.println("-------------recursiveFindLeaf-------------------");
-		Set<Integer> keys = mapEnfantParent.keySet(); //key=enfant ,parent
-		for (Integer key : keys) {
-			System.out.println("---" + mapEnfantParent.get(key) + "<->" + key);
 
-		}
-
-		// { 1 :[{1,2,3}],2...
-		int gardeMyVar = 1;
-		System.out.print("{");
-		for (Integer key : keys) {
-
-			if (mapEnfantParent.get(key) == gardeMyVar) {
-				System.out.print("{" + key + "}");
-			} else {
-				System.out.print(mapEnfantParent.get(key));
-				gardeMyVar = mapEnfantParent.get(key);
-			}
-			//System.out.print(",{");
-
-		}
-		System.out.print("}");
-		// ------------
-		
-	}
 	
-	public static List<Object> recursiveFindLeafFinal(Map<String, Object> mapEnfantParent) {
-		 	List<Object> retVal = new ArrayList<Object>();
-		    for (Map.Entry<String, Object> entry : mapEnfantParent.entrySet()) {
-		        Object value = entry.getValue();	
-		        if (value instanceof Map) {
-		            retVal.addAll(recursiveFindLeafFinal((Map) value));
-		        } else {
-		            retVal.add(value);
-		        }
-		    }
-		    return retVal;
-	}
+
 	
-	static String finalJson ="{\"name\":\""+1+"\",\"children\":[";
+	/**
+	 * Méthode récursive qui construit un json en fonction parent rentré
+	 */
+	static String finalJson ="{\"name\":\""+1+"\",\"children\":["; // a déplacer dans une nouvelle méthode <<<<<<
 	public static void FinalRecursivee(int parent ,Map<Integer, Integer> mapEnfantParent){
 		
 		//System.out.print("{'name':'"+parent+"','children':[");
@@ -242,71 +180,53 @@ public class Main {
 				
 			}
 		}
-		
-		
+
 		finalJson = finalJson +"]}";
 		//System.out.print("]},");
 	}
 
-	public static void recursiveSql(Connection c) {
-		Statement stmt = null;
-		try {
-			String sqlre = "WITH RECURSIVE q AS (SELECT id_node_1, id_node FROM composition WHERE id_node_1 = 4 UNION SELECT m.id_node_1, m.id_node FROM composition m JOIN q ON q.id_node_1 = m.id_node) SELECT id_node_1, id_node FROM q;";
 
-			stmt = c.createStatement();
-			ResultSet rs = stmt.executeQuery(sqlre);
-			boolean lock = true;
-			int oldIdNode = 1;
-			//rs.next();
-			//Integer TestType = rs.getInt(1);
-			//System.out.println(TestType);
-			while (rs.next()) {
-				//Version de base
-				int id_parent = rs.getInt("id_node");
-				int id_enfant = rs.getInt("id_node_1");
-				System.out.print(id_parent);
-				System.out.print(" <-> " + id_enfant);
-				System.out.println();
-				//---------------
+	/* ********************************
+	 * 			POUR LES TESTS
+	 * ********************************
+	 */
+	
+	//TEST PAS BON
+	public static void recursiveFindLeaf(Map<Integer, Integer> mapEnfantParent) {
+		System.out.println("-------------recursiveFindLeaf-------------------");
+		Set<Integer> keys = mapEnfantParent.keySet(); //key=enfant ,parent
+		for (Integer key : keys) {
+			System.out.println("---" + mapEnfantParent.get(key) + "<->" + key);
 
-				/*if (lock == true) {
-					oldIdNode = id_parent;
-					lock = false;
-					System.out.println("lock");
-				}
-
-				System.out.println(oldIdNode + "^^" + rs.getInt("id_node"));
-				if (oldIdNode == rs.getInt("id_node")) {
-					oldIdNode = rs.getInt("id_node");
-					System.out.println("-------->" + oldIdNode);
-				}*/
-
-			}
-			rs.close();
-			stmt.close();
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
 		}
-		System.out.println("SelectNode done successfully");
+		// { 1 :[{1,2,3}],2...
+		int gardeMyVar = 1;
+		System.out.print("{");
+		for (Integer key : keys) {
 
+			if (mapEnfantParent.get(key) == gardeMyVar) {
+				System.out.print("{" + key + "}");
+			} else {
+				System.out.print(mapEnfantParent.get(key));
+				gardeMyVar = mapEnfantParent.get(key);
+			}
+			//System.out.print(",{");
+
+		}
+		System.out.print("}");
 	}
-
-
-
-	// http://www.prodigyproductionsllc.com/articles/programming/recursively-iterate-hashmap-with-java/
-	// http://stackoverflow.com/questions/9337536/iterate-recursively-through-deep-hashmap
-	//-------TEST 3 récursive 
-	/*private static void iterateHashMap(HashMap<Integer, Object> map) {
-		Iterator it = map.keySet().iterator();
-		System.out.println("-");
-		while (it.hasNext()) {
-			Integer key = (Integer) it.next();
-			System.out.println("---" + map.get(key) + "<->" + key);
-			if (map.get(key) instanceof HashMap) {
-				iterateHashMap((HashMap) map.get(key));
-			}
-		}
-	}*/
-
+	
+	//TEST PAS BON
+	public static List<Object> recursiveFindLeafFinal(Map<String, Object> mapEnfantParent) {
+	 	List<Object> retVal = new ArrayList<Object>();
+	    for (Map.Entry<String, Object> entry : mapEnfantParent.entrySet()) {
+	        Object value = entry.getValue();	
+	        if (value instanceof Map) {
+	            retVal.addAll(recursiveFindLeafFinal((Map) value));
+	        } else {
+	            retVal.add(value);
+	        }
+	    }
+	    return retVal;
+}
 }
