332cc8a4f1aedab2246e0f937235c1a97ef86006
Erozbliz <erozbliz@hotmail.com>
2016-12-01 13:07:59 -0500

    parent dans l'URL OK
    http://127.0.0.1:9999/getSunburst?parent=1

--- test_postgres/Handler.java
+++ test_postgres/Handler.java
@@ -2,8 +2,12 @@ package test_postgres;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -14,10 +18,8 @@ import com.sun.net.httpserver.HttpHandler;
  * http://www.java2s.com/Code/Jar/h/Downloadhttp20070405jar.htm
  *
  */
-public class Handler{
-	
-	
-	
+public class Handler {
+
 	/**
 	 * Route /
 	 */
@@ -31,27 +33,45 @@ public class Handler{
 			os.close();
 		}
 	}
-	
-	
+
 	/**
 	 * Permet d'avoir le dernier evenement
+	 * http://127.0.0.1:9999/getSunburst?parent=0
 	 * Route  /getSunburst
 	 */
 	public static class GetSunburstHandler implements HttpHandler {
 		@Override
 		public void handle(HttpExchange argHttpExhange) throws IOException {
+
+			// parse request
+			Map<String, String> parameters = new HashMap<String, String>();
+			//Message a afficher
+			String responseStr = "";
+			int parentId = 0;
+
 			URI requestedUri = argHttpExhange.getRequestURI();
 			String query = requestedUri.getRawQuery();
 
-			String responseStr = "JSON";
-			
+			if (query != null) {
+				//On cherche les arguments passé dans l'url
+				parameters = queryToMap(query);
+				//System.out.println("query=" + query);
+				System.out.println("param parent=" + parameters.get("parent"));
+
+				parentId = Integer.parseInt(parameters.get("parent"));
+				//System.out.println("param parent=" + parentId);
+			} else {
+				System.out.println("Aucun argument (ex:/getSunburst?parent=0)");
+			}
+
 			//Lancement du script sql
-			responseStr = SqlRequest.SelectAll(Main.c, 1);
-			
+			System.out.println("parentId " + parentId);
+			responseStr = SqlRequest.SelectAll(Main.c, parentId);
+
 			//Map<String, String> params = queryToMap(query); 
 			//System.out.println("user=" + params.get("user"));
 			//System.out.println(argHttpExhange.getRequestURI().getQuery());
-			
+
 			argHttpExhange.getResponseHeaders().add("Access-Control-Allow-Origin", "*"); //CORS
 			argHttpExhange.sendResponseHeaders(200, responseStr.length());
 			OutputStream os = argHttpExhange.getResponseBody();
@@ -59,27 +79,73 @@ public class Handler{
 			os.close();
 		}
 	}
-	
-	
+
+	/**
+	 * 
+	 *  TOOL
+	 * 
+	 */
+
 	/**
 	 * Deserialisation URL pour avoir les parametres
 	 * 
 	 * Utilisation : Map<String, String> params = queryToMap(httpExchange.getRequestURI().getQuery()); 
+	 * System.out.println("param A=" + params.get("A"));
 	 * @param query
 	 * @return
 	 */
-	public static Map<String, String> queryToMap(String query){
-	    Map<String, String> result = new HashMap<String, String>();
-	    for (String param : query.split("&")) {
-	        String pair[] = param.split("=");
-	        if (pair.length>1) {
-	            result.put(pair[0], pair[1]);
-	        }else{
-	            result.put(pair[0], "");
-	        }
-	    }
-	    return result;
+	public static Map<String, String> queryToMap(String query) {
+		Map<String, String> result = new HashMap<String, String>();
+		for (String param : query.split("&")) {
+			String pair[] = param.split("=");
+			if (pair.length > 1) {
+				result.put(pair[0], pair[1]);
+			} else {
+				result.put(pair[0], "");
+			}
+		}
+		return result;
 	}
 
+	/**
+	 * 
+	 * @param query
+	 * @param parameters
+	 * @throws UnsupportedEncodingException
+	 */
+	public static void parseQuery(String query, Map<String, Object> parameters) throws UnsupportedEncodingException {
+
+		if (query != null) {
+			String pairs[] = query.split("[&]");
+			for (String pair : pairs) {
+				String param[] = pair.split("[=]");
+				String key = null;
+				String value = null;
+				if (param.length > 0) {
+					key = URLDecoder.decode(param[0], System.getProperty("file.encoding"));
+				}
+
+				if (param.length > 1) {
+					value = URLDecoder.decode(param[1], System.getProperty("file.encoding"));
+				}
+
+				if (parameters.containsKey(key)) {
+					Object obj = parameters.get(key);
+					if (obj instanceof List<?>) {
+						List<String> values = (List<String>) obj;
+						values.add(value);
+
+					} else if (obj instanceof String) {
+						List<String> values = new ArrayList<String>();
+						values.add((String) obj);
+						values.add(value);
+						parameters.put(key, values);
+					}
+				} else {
+					parameters.put(key, value);
+				}
+			}
+		}
+	}
 
 }
